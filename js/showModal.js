42c5fa58f6f3746cf5f526eb3a47ef4b09bca195
truc2203 <tranthanhtruc22031998@gmail.com>
2022-10-15 17:38:48 +0700

    fix css

--- js/showModal.js
+++ js/showModal.js
@@ -1,10 +1,12 @@
 const showModal = () => {
     let isOpen = document.getElementById('modal_video');
-    isOpen.style.visibility = 'visible'
+    isOpen.style.display = 'block'
     isOpen.style.transition = 'all 0.5s'
+
 }
 
 const closeModal = () => {
     let onClose = document.getElementById('modal_video')
-    onClose.style.visibility = 'hidden'
+    onClose.style.display = 'none'
+
 }
